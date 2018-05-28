a6be26dfa080b9920bc2afcf13a3a89c5c1c7966
Poonyawee Visetputtasart <59090023@kmitl.ac.th>
2018-05-28 13:43:38 +0700

    Fix UI Bug

--- EzLibrarianApplication/SmartLib_LibrarianUI.py
+++ EzLibrarianApplication/SmartLib_LibrarianUI.py
@@ -508,7 +508,7 @@ class Ui_MainWindow(object):
         self.toolBar.addSeparator()
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(2)
+        self.tabWidget.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
